36fc673b5d88565b6d7bf1b05eea5e4ced94ff15
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-02-08 14:13:28 +0200

    bc

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import nodemon from 'nodemon';
-import  db  from "./db/connection.js"
-// const express =require('express')
-const app =express()
-app.use(express)
-app.get('/',(req,res)=>{
-res.send("Server started");
-// res.end();
-})
-app.listen(4000,()=>{
-     console.log('Server is running on http://localhost:4000')
-})
+import connection from './db/connection.js';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Server started");
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on http://localhost:4000');
+});
